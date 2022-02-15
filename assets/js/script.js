56d2d0fb26b723d127ff5821b9f5309355bd6e52
Ulugbek <79435067+Chelovek2343@users.noreply.github.com>
2022-02-15 15:56:02 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,64 +48,25 @@ async function showCountry() {
     loc.textContent = `${data.time_zone}`;
 }
 
-// async function getTime() {
-//     const location = document.getElementById('location');
-
-//     const responsive = await fetch('http://worldtimeapi.org/api/ip');
-//     const timeData = await responsive.json();
-
-//     const currentTime = timeData.datetime;
-
-//     time.innerHTML = `${currentTime.slice(
-//         11,
-//         16
-//     )} <span>${timeData.utc_offset.slice(0, 3)}</span>`;
-
-//     location.textContent = timeData.timezone;
-//     dayYear.textContent = timeData.day_of_year;
-//     dayWeek.textContent = timeData.day_of_week;
-//     weekNumber.textContent = timeData.week_number;
-
-//     if (time >= '5' && time <= '11') {
-//         main.style.backgroundImage = 'url(../assets/img/morning.jpg)';
-//         textDate.textContent = `Good Morning. It's currently`;
-//         icon.innerHTML = `<?xml version="1.0" ?><svg fill="none" height="24" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17 18a5 5 0 0 0-10 0"/><line x1="12" x2="12" y1="2" y2="9"/><line x1="4.22" x2="5.64" y1="10.22" y2="11.64"/><line x1="1" x2="3" y1="18" y2="18"/><line x1="21" x2="23" y1="18" y2="18"/><line x1="18.36" x2="19.78" y1="11.64" y2="10.22"/><line x1="23" x2="1" y1="22" y2="22"/><polyline points="8 6 12 2 16 6"/></svg>`;
-//     } else if (time >= '12' && time <= '17') {
-//         main.style.backgroundImage = 'url(../assets/img/afternoon.jpg)';
-//         textDate.textContent = `Good Afternoon. It's currently`;
-//         icon.innerHTML = `<i class="fas fa-sun"></i>`;
-//     } else {
-//         main.style.backgroundImage = 'url(../assets/img/night.jpg)';
-//         textDate.textContent = `Good Evening. It's currently`;
-//         icon.innerHTML = `<i class="fas fa-moon"></i>`;
-//     }
-//     setInterval(getTime, 1000);
-// }
-
 function getTime() {
-    const getDate = Date();
+    const getDate = new Date();
+    const getHour = getDate.getHours();
+    const getMin = getDate.getMinutes();
+    const getTimeZone = -(getDate.getTimezoneOffset() / 60);
 
-    const newDate = getDate.slice(16, 21);
-    const timeZone = getDate.slice(28, 31);
+    time.innerHTML = `${getHour}:${getMin} <span>+${getTimeZone}</span>`;
 
-    time.innerHTML = `${newDate} <span>${timeZone}</span>`;
-
-    if (time <= 12) {
+    if (getHour <= 12) {
         main.style.backgroundImage =
             'url(https://images.unsplash.com/photo-1551104083-3b336cfd4dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)';
         textDate.textContent = `Good Morning. It's currently`;
         icon.innerHTML = `<?xml version="1.0" ?><svg fill="none" height="24" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17 18a5 5 0 0 0-10 0"/><line x1="12" x2="12" y1="2" y2="9"/><line x1="4.22" x2="5.64" y1="10.22" y2="11.64"/><line x1="1" x2="3" y1="18" y2="18"/><line x1="21" x2="23" y1="18" y2="18"/><line x1="18.36" x2="19.78" y1="11.64" y2="10.22"/><line x1="23" x2="1" y1="22" y2="22"/><polyline points="8 6 12 2 16 6"/></svg>`;
-    } else if (time <= 18) {
+    } else if (getHour <= 18) {
         main.style.backgroundImage =
             'url(https://images.unsplash.com/photo-1530295314625-30d3b777ac7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80)';
         textDate.textContent = `Good Afternoon. It's currently`;
         icon.innerHTML = `<i class="fas fa-sun"></i>`;
-    } else if (time >= 18) {
-        main.style.backgroundImage =
-            'url(https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1513&q=80)';
-        textDate.textContent = `Good Evening. It's currently`;
-        icon.innerHTML = `<i class="fas fa-moon"></i>`;
-    } else {
+    } else if (getHour >= 18) {
         main.style.backgroundImage =
             'url(https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1513&q=80)';
         textDate.textContent = `Good Evening. It's currently`;
@@ -114,65 +75,6 @@ function getTime() {
 }
 
 setInterval(getTime, 1000);
-
-// dateName();
-
-// function dateName(time) {
-//     let mainTime = new Date();
-
-//     // const newDate = mainTime.slice(16, 21);
-//     // const timeZone = mainTime.slice(28, 31);
-
-//     time.innerHTML = `${mainTime} <span>${mainTime}</span>`;
-
-//     // if (time <= 12) {
-//     //     return `Доброе утро ${name}`;
-//     // } else if (time <= 18) {
-//     //     return `Добрый день ${name}`;
-//     // } else if (time >= 18) {
-//     //     return `Добрый вечер ${name}`;
-//     // } else {
-//     //     return `Доброй ночи ${name}`;
-//     // }
-// }
-
-// function getTime() {
-//     const location = document.getElementById('location');
-
-//     axios
-//         .get('https://worldtimeapi.org/api/ip')
-//         .then((regionRes) => {
-//             const timeData = regionRes.data;
-//             const currentTime = timeData.datetime;
-
-//             time.innerHTML = `${currentTime.slice(
-//                 11,
-//                 16
-//             )} <span>${timeData.utc_offset.slice(0, 3)}</span>`;
-
-//             location.textContent = timeData.timezone;
-//             dayYear.textContent = timeData.day_of_year;
-//             dayWeek.textContent = timeData.day_of_week;
-//             weekNumber.textContent = timeData.week_number;
-
-//             if (time >= '5' && time <= '11') {
-//                 main.style.backgroundImage = 'url(../assets/img/morning.jpg)';
-//                 textDate.textContent = `Good Morning. It's currently`;
-//                 icon.innerHTML = `<?xml version="1.0" ?><svg fill="none" height="24" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M17 18a5 5 0 0 0-10 0"/><line x1="12" x2="12" y1="2" y2="9"/><line x1="4.22" x2="5.64" y1="10.22" y2="11.64"/><line x1="1" x2="3" y1="18" y2="18"/><line x1="21" x2="23" y1="18" y2="18"/><line x1="18.36" x2="19.78" y1="11.64" y2="10.22"/><line x1="23" x2="1" y1="22" y2="22"/><polyline points="8 6 12 2 16 6"/></svg>`;
-//             } else if (time >= '12' && time <= '17') {
-//                 main.style.backgroundImage = 'url(../assets/img/afternoon.jpg)';
-//                 textDate.textContent = `Good Afternoon. It's currently`;
-//                 icon.innerHTML = `<i class="fas fa-sun"></i>`;
-//             } else {
-//                 main.style.backgroundImage = 'url(../assets/img/night.jpg)';
-//                 textDate.textContent = `Good Evening. It's currently`;
-//                 icon.innerHTML = `<i class="fas fa-moon"></i>`;
-//             }
-//         })
-//         .catch((err) => console.error(err));
-
-//     setInterval(getTime, 1000);
-// }
 
 more.addEventListener('click', () => {
     timeMain.classList.toggle('active');
